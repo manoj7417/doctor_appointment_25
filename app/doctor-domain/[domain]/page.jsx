6491648a14c6209713622975c024931d5b2ae6f6
manoj7417 <manoj@glassfrog.design>
2025-09-02 16:46:45 +0530

    changes in doctor domain page

--- app/doctor-domain/[domain]/page.jsx
+++ app/doctor-domain/[domain]/page.jsx
@@ -174,7 +174,7 @@ export default function DoctorDomainPage() {
       </div> */}
 
       {/* Main Content */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Doctor Info Card */}
           <div className="lg:col-span-1">
